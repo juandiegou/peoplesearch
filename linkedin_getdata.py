77b1c173edff87f67300c8545f48d8c560af14c1
DavidCDCB <cristian-cb1@hotmail.com>
2022-11-10 20:36:30 -0500

    Busqueda de persona según terminos clave

--- linkedin_getdata.py
+++ linkedin_getdata.py
@@ -20,18 +20,46 @@ def check_element(criterio,id):
 def check_elements(criterio,id):
 	return WebDriverWait(driver, timeout=50).until(lambda d: d.find_elements(criterio,id))
 
+def find_word(text):
+    concordance = 0
+    for key in keys:
+        if(key.lower() in text.lower()):
+            concordance += 1
+        if(concordance >= 1 and keys[0].lower() in text.lower()):
+            print("\nEncontrad@ con prioridad")
+            return True
+        if(concordance >= 3):
+            print("\nEncontrad@ con cantidad")
+            return True
+    return False
+
+def find_persons(first_name, last_name):
+    links_persons = []
+
+    check_element(By.NAME,"firstName").send_keys(first_name)
+    check_element(By.NAME,"lastName").send_keys(last_name)
+    t.sleep(1)
+    check_element(By.CLASS_NAME,"base-search-bar__submit-btn").click()
+    t.sleep(1)
+    persons = check_elements(By.XPATH,"//section[@class='serp-page__results-list']/child::ul/child::li")
+
+    for p in persons:
+        link = p.find_element(By.TAG_NAME,"a")
+        if(find_word(link.text)):
+            links_persons.append(link.get_attribute('href'))
+            print(link.get_attribute('href'))
+            print(link.text)
+
+    return links_persons
+
+
 if(sys.platform.startswith('linux')):
     driver = webdriver.Firefox(executable_path='./geckodriver')
 else:
-  driver = webdriver.Firefox(executable_path='.\\chromedriver') 
-  #driver = webdriver.Chrome(executable_path='.\\chromedriver.exe')
+    driver = webdriver.Firefox(executable_path='.\\chromedriver.exe')
 
 driver.get('https://co.linkedin.com/')
 t.sleep(3)
 check_element(By.XPATH,"//a[@href='https://www.linkedin.com/pub/dir/+/+?trk=guest_homepage-basic_guest_nav_menu_people']").click()
 
-check_element(By.NAME,"firstName").send_keys('Simple')
-check_element(By.NAME,"lastName").send_keys('Kumar')
-t.sleep(1)
-check_element(By.CLASS_NAME,"base-search-bar__submit-btn").click()
-
+find_persons('Simple', 'Kumar')
